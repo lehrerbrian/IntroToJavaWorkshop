42498f1f049e9b82404c513fe5bf85329fc099d3
lehrerbrian <league@WTS19.attlocal.net>
2017-08-22 11:56:18 -0700

    workshop day2

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,21 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
-	
-		
+System.out.println("hello world!");
+Robot  robot485= new Robot("mini");	
+		robot485.moveTo(250, 189);
+robot485.penDown();
+robot485.setPenColor(Color.CYAN);		
+robot485.move(110);		
+robot485.turn(90);
+robot485.setPenColor(Color.GREEN);
+robot485.move(110);		
+robot485.turn(90);
+robot485.setPenColor(Color.YELLOW);
+robot485.move(110);		
+robot485.turn(90);
+robot485.setPenColor(Color.black);
+robot485.move(110);		
 		
 	}
 }
--- src/day1/robot/TriangleShell.java
+++ src/day1/robot/TriangleShell.java
@@ -5,32 +5,44 @@ import org.jointheleague.graphical.robot.Robot;
 public class TriangleShell {
 
 	// 1. Create a new Robot
-
+	Robot  robot485= new Robot("mini");
 	
 	void go() {
-		drawTriangle(100); // 3. delete this line (used only for testing)
+		 // 3. delete this line (used only for testing)
 
 		// 6. Make the robot go as fast as possible
-
+		robot485.setSpeed(10);	
 		// 4. make a variable to hold the length of the triangle and set it to 50
-
+int length=50;
 		// 7. Do the following (up to step 10) 60 times
 
-			// 9. Change the color of the pen to a random color
+for(int i=0;i<60;i++) {
 	
-			// 8. Increase the length of the side by 10 pixels
-	
-			// 5. call your drawTriangle() method using your length variable
-	
-			// 10. Turn the tortoise 6 degrees to the right
 
-	}
+			// 9. Change the color of the pen to a random color
+	robot485.setRandomPenColor();
+			// 8. Increase the length of the side by 10 pixels
+	length+=10;
+			// 5. call your drawTriangle() method using your length variable
+drawTriangle(length);
+			// 10. Turn the tortoise 6 degrees to the right
+robot485.turn(6);
+}
+}
 
 	/* 2. fill in the method below to draw a triangle. Use the length variable for the size of the triangle. */
 	private void drawTriangle(int length) {
-		
-		
-		
+	
+	robot485.turn(30);	
+	robot485.penDown();
+	robot485.move(length);robot485.setSpeed(10);	
+	robot485.turn(120);	
+	robot485.penDown();
+	robot485.move(length);
+	robot485.turn(120);
+	robot485.penDown();
+	robot485.move(length);
+	
 	}
 
 	
